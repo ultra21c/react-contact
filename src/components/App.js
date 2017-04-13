176eb52bb134ad487006f2f5cfd2c26169bdd0ec
steve.jung <steve.jung@daumkakao.com>
2017-04-13 20:26:39 +0900

    react skeleton

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      name: ''
+    }
+  }
+  render(){
+        return (
+          <div>
+            <button onClick={() => { this.setState({name: 'steve.jung'}); }}>CLICK ME</button>
+            <h1>Hello!! React ^^;; ({this.state.name})</h1>
+          </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        hot: true,
+        inline: true,
+        host: '0.0.0.0',
+        port: 4000,
+        contentBase: __dirname + '/public/',
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot-loader', 'babel-loader?' + JSON.stringify({
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                })
+
+              ],
+                exclude: /node_modules/
+                // query: {
+                //     cacheDirectory: true,
+                //     presets: ['es2015', 'react']
+                // }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
